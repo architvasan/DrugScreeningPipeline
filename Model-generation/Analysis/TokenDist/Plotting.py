85890b2a45aec30a8f37849e19a362f1217bacfb
Archit <akvasan2@illinois.edu>
2024-01-23 23:08:48 +0000

    Plotting comparison of token dists

--- Model-generation/Analysis/TokenDist/Plotting.py
+++ Model-generation/Analysis/TokenDist/Plotting.py
@@ -4,12 +4,29 @@ import matplotlib
 import pandas as pd
 matplotlib.use("Agg")
 import json
-#from SmilesPE.tokenizer import *
-from smiles_pair_encoders_functions import *
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+font = {'family' : 'normal',
+    'weight' : 'normal',
+    'size'   : 16}
 
-penguins = pd.read_csv('token_dist.csv')[:30]
-sns.barplot(penguins, x="Weight", y="Token")#, legend=False)
-plt.savefig('token_dist.png', dpi=300, bbox_inches='tight')
+matplotlib.rc('font', **font)
+cmap="RdBu_r"
+
+penguins = pd.read_csv('token_dist.csv')[:15]
+penguins['Tot_comp']=[0 for i in range(len(penguins))]
+df_tot = pd.read_csv('token_dist_tot_comb.csv')
+df_tot_vals = []
+for tok in penguins['Token']:
+    print(df_tot[df_tot['Token']==tok]['Weight'].tolist()[0])
+    df_tot_vals.append(df_tot[df_tot['Token']==tok]['Weight'].tolist()[0])
+
+penguins['Tot_comp']=df_tot_vals
+penguins.to_csv('data_comp.csv',index=False)
+penguins.plot.barh(x='Token', stacked=True,figsize=(10,20), color=['red', 'gray'])
+#penguins.plot(kind='bar', x=["Weight", "Tot_comp"], y="Token")#, stacked=True)#, legend=False)
+plt.xlabel('Weight')
+plt.ylabel('Token')
+plt.savefig('token_dist_comp.png', dpi=300, bbox_inches='tight')
+plt.close()
